4458edd62bc40d857e83a2a1a23f0634f35255f2
Alfredo Gonzalez <alfredo.gg117@yahoo.com>
2019-02-21 04:59:03 -0800

    more organized methods

--- deck.js
+++ deck.js
@@ -8,8 +8,9 @@ class Deck{
     let card = (value, suit) =>{
       this.value = value;
       this.suit = suit;
+      this.show = true;
       this.cardName = value + ' of ' + suit;
-      return {cardName:this.cardName, suit:this.suit, value:this.value}
+      return {cardName:this.cardName, suit:this.suit, value:this.value, show:this.show}
     }
     let values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
     let suites = ['♣','♦','♠','♥']
@@ -21,69 +22,43 @@ class Deck{
     }
   }
 
-  draw(){
-    //function returns value and suit of top card.
-    /*
-    function lastCard(theDeck){
-      return [theDeck[theDeck.length - 1].value,theDeck[theDeck.length - 1].suit];
-    }
-    //starts loop to create 2 cards
-    var parent = document.getElementsByClassName("container")[0]
+  showCard(div, idSlot, card){
 
+    //creates a value div w/ textnode value and id
+    var divValue = document.createElement("div");
+    divValue.id = "value";
+    var nodeValue = document.createTextNode(card.value);
+    divValue.appendChild(nodeValue);
+    //creates a suit div w/ textnode suit and id
+    var divSuit = document.createElement("div");
+    divSuit.id = "suit";
+    var nodeSuit = document.createTextNode(card.suit);
+    divSuit.appendChild(nodeSuit);
 
-    let playerHand = (card1, card2) =>{
-      this.card1 = card1;
-      this.card2 = card2;
-      return {card1:this.card1, card2:this.card2}
-    }
-
-
-
-
-
-  for(var j = 0; j < 6; j++){
-    this.player[j] = [];
-    for(var i = 0; i <2; i++){
-      //creates a blank card
-      var div = document.createElement("div");
-      div.className = "blank";
-      //creates a value div w/ textnode value and id
-      var divValue = document.createElement("div");
-      divValue.id = "value";
-      var nodeValue = document.createTextNode(lastCard(this.deck)[0]);
-      divValue.appendChild(nodeValue);
-
-      //creates a suit div w/ textnode suit and id
-      var divSuit = document.createElement("div");
-      divSuit.id = "suit";
-      var nodeSuit = document.createTextNode(lastCard(this.deck)[1]);
-      divSuit.appendChild(nodeSuit);
-      //appends the value div and suit div to the blank div
+    //appends the value div and suit div to the blank div
       div.appendChild(divValue);
       div.appendChild(divSuit);
-      parent.appendChild(div);
 
-      this.player[j].push(lastCard(this.deck));
-      console.log(this.player[j][i]);
-      this.deck.pop();
-      console.log(this.deck[2].cardName);
-
-    }
-
+    idSlot.appendChild(div);
+    console.log("what");
   }
 
 
+  placeCard(idSlot, card){
+      //creates a blank card
+    var div = document.createElement("div");
+    div.className = "blank";
 
-    //if(this.deck.length < 1)return document.getElementById("new").innerHTML = 'Out of cards';
-    //cardId("value").innerHTML = last(this.deck).value;
-    //cardId("suit").innerHTML =  last(this.deck).suit;
-    //this.deck.pop();
+    if(card.show == true){
 
-    //cardId("value2").innerHTML = last(this.deck).value;
-    //cardId("suit2").innerHTML = last(this.deck).suit;
-    //this.deck.pop();
-*/
+    deck.showCard(div, idSlot,card);
+
   }
+  else{idSlot.appendChild(div);}
+
+
+  }
+
 
   playerHands(){
 
@@ -100,34 +75,17 @@ class Deck{
 
   fillSlots(){
 
-
     for(var i=0; i<8; i++){
       let parent = document.getElementById(i);
       for(var j=0; j<2; j++){
-
-        //creates a blank card
-        var div = document.createElement("div");
-        div.className = "blank";
-        //creates a value div w/ textnode value and id
-
-        if(i == 0){
-        var divValue = document.createElement("div");
-        divValue.id = "value";
-        var nodeValue = document.createTextNode(this.player[i][j].value);
-        divValue.appendChild(nodeValue);
-        //creates a suit div w/ textnode suit and id
-        var divSuit = document.createElement("div");
-        divSuit.id = "suit";
-        var nodeSuit = document.createTextNode(this.player[i][j].suit);
-        divSuit.appendChild(nodeSuit);
-        //appends the value div and suit div to the blank div
-        div.appendChild(divValue);
-        div.appendChild(divSuit);
-      }
-        parent.appendChild(div);
+        if(i!=0)this.player[i][j].show = false;
+        console.log(this.player[i][j].show)
+      deck.placeCard(parent ,this.player[i][j]);
 
       }
+
     }
+
   }
 
 
@@ -153,6 +111,8 @@ class Deck{
 }
 
 
+
+
 //end of classes
 
 
@@ -163,5 +123,9 @@ deck.makeDeck();
 deck.shuffle();
 deck.playerHands();
 deck.fillSlots();
+
+}
+
+function next(){
 
 }
